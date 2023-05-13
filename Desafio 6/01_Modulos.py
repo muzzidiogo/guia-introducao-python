ffc50ace7aec3a99faf1674cbc620ba9cb81c9f4
Diogo Muzzi <muzzidiogo@gmail.com>
2023-05-13 01:53:19 +0000

    #007 - Bibliotecas

--- Desafio 6/01_Modulos.py
+++ Desafio 6/01_Modulos.py
@@ -4,19 +4,23 @@ import sys
 
 # Printar o Sistema Operacional que voce esta usando:
 # YOUR CODE HERE
+print(f'Sistema Operacional: {sys.platform}')
 
 # Printar a versão de Python que voce esta usando:
 # YOUR CODE HERE
-
+print(f'Versão do python: {sys.version_info}')
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Printar a process Id atual
 # YOUR CODE HERE
+print(f'Process ID Atual: {os.getpid()}')
 
 # Printar o atual diretório:
 # YOUR CODE HERE
+print(f'Diretório atual: {os.curdir}')
 
 # Printar o nome da maquina
 # YOUR CODE HERE
+print(f'Nome desta máquina na rede: {os.uname()}')
--- Desafio 6/02_Numpy.py
+++ Desafio 6/02_Numpy.py
@@ -1,36 +1,53 @@
-
 import numpy as np
 
 #1- Crie uma matriz 1D com números de 0 a 9
-
+matrix = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(f'#1: {matrix}\n')
 
 #2- Crie uma matriz booleana numpy 3×3 com ‘True’
-
+matrix = np.array([[True, True, True],
+                   [True, True, True],
+                   [True, True, True]])
+print(f'#2: \n{matrix}\n')
 
 #3- Extraia todos os números ímpares de ‘arr’
-
 arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-#4- Substitua todos os números ímpares arr por -1
+index = 0
+indices = []
 
+for i in np.nditer(arr):
+    if i % 2 != 0:
+        indices.append(index)
+    index+=1
+
+print(f'#3: ìmpares nas posições de indice {indices}\n')
+
+#4- Substitua todos os números ímpares arr por -1
+arr[indices] = -1
+print(f'#4: {arr}\n')
 
 #5- Substitua todos os números ímpares em arr com -1 sem alterar arr
-
-arr = np.arange(10)
-#out = np.where()
-
-#6- Converta uma matriz 1D para uma matriz 2D com 2 linhas:
-
 arr = np.arange(10)
 
-       
-#6- Empilhe matrizes verticalmente:
+arr2 = np.where(arr % 2 != 0)
+print(f'#5: {arr2}\n')
+#6 - Converta uma matriz 1D para uma matriz 2D com 2 linhas:
+arr = np.arange(10)
 
+arr2D = arr[np.newaxis, :]
+print(f'#6: \n{arr2D}\n')
+
+#7- Empilhe matrizes verticalmente:
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
 
+m_vertical = np.vstack((a, b))
+print(f'#7: \n{m_vertical}\n')
 
-#6- Empilhe as matrizes horizontalmente:
-
+#8- Empilhe as matrizes horizontalmente:
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
+
+m_horizontal = np.hstack((a, b))
+print(f'#8: \n{m_horizontal}\n')
