2b3334254ab7cfe4e00d7b40facd5d59357d0c02
Diogo Muzzi <muzzidiogo@gmail.com>
2023-05-13 02:26:41 +0000

    #008 - Arquivos

--- Desafio 7/01_File.py
+++ Desafio 7/01_File.py
@@ -4,9 +4,15 @@ referencia: https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writ
 
 # 1-Abra o arquivo do diretorio atual "foo.txt"
 # Printe todo o conteudo do arquivo , então feche o arquivo
+with open('my_workspace/guia-introducao-python/Desafio 7/foo.txt', 'r') as foo:
+    print(f'#1: {foo.read()}\n')
 
 # 2- Crie um arquivo chamado "bar.txt" 
 # Abra o arquivo e conte quanta vezes a palavra "sir" aparece 
 # Escreva no arquivo que voce criou quantas palavras foram encontradas
 # Feche o arquivo
-
+with open('my_workspace/guia-introducao-python/Desafio 7/foo.txt', 'r') as foo:
+    texto = foo.read()
+    num_sir = texto.count('sir')
+    with open('my_workspace/guia-introducao-python/Desafio 7/bar.txt', 'w') as bar:
+        bar.write(f'A palavra "sir" foi escrita {num_sir} vezes no arquivo "foo.txt"')
